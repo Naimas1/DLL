91286fd22db6fc43704e7b217620f1be322bdbd9
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-12-09 21:40:21 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace DLL
+using System;
+
+namespace GameLibrary
 {
-    internal class Program
+    public class Game
     {
-        static void Main(string[] args)
+        // Властивості для зберігання інформації про гру
+        public string Title { get; set; }
+        public string Studio { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        // Конструктор класу
+        public Game(string title, string studio, string genre, DateTime releaseDate)
         {
-            Console.WriteLine("Hello, World!");
+            Title = title;
+            Studio = studio;
+            Genre = genre;
+            ReleaseDate = releaseDate;
         }
-    }
-}
+
+        // Метод для виведення інформації про гру
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Назва гри: {Title}");
+            Console.WriteLine($"Студія: {Studio}");
+            Console.WriteLine($"Стиль гри: {Genre}");
+            Console.WriteLine($"Дата релізу: {ReleaseDate.ToShortDateString()}");
+
+
